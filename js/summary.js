28d0297b5b4073209379d316602577f9e918332b
Jaime Alvarez <jaimealonso.alvarez1984@gmail.com>
2023-05-20 19:59:44 -0500

    First commit

---
+++ js/summary.js
@@ -0,0 +1,34 @@
+const origin = window.location.origin;
+
+fetch(`${origin}/js/data.json`)
+  .then((response) => response.json())
+  .then((data) => {
+    let scoreResult = 0;
+    data.forEach((item) => {
+      scoreResult += item.score;
+      document.getElementById("items").appendChild(itemTemplate({ ...item }));
+    });
+    document.getElementById("scoreResult").innerText = Math.floor(
+      scoreResult / data.length
+    );
+  });
+
+function itemTemplate({ category, icon, score }) {
+  const itemHTML = `
+    <div class="item-name">
+        <img src="${icon}" alt="${category}" />
+        <span>${category}</span>
+    </div>
+    <div class="item-qualifications">
+        <span>${score}</span>
+        <span>/</span>
+        <span>100</span>
+    </div>
+    `;
+  const element = document.createElement("div");
+  element.classList.add("item");
+  element.classList.add(category.toLowerCase());
+  element.innerHTML = itemHTML;
+
+  return element;
+}
